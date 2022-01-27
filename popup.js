6c123d912eb64d4920f8fa89587a65adc7d41308
Leonoor Rinke de Wit <lrinkedewit@gmail.com>
2022-01-26 21:00:22 -0500

    newest changes

---
+++ popup.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    class Penguin {
+        constructor(el) {
+            this.node = document.createElement("img");
+            this.node.setAttribute("id", "penguin");
+            this.node.setAttribute("src", "src/access/Penguin-24px.png");
+        
+            el.appendChild(this.node);
+        
+        }
+    }  
+    
+    // DOM Elements
+    // body
+    const body = document.querySelector("body");
+
+    // 1. div birthdayPerson
+    const birthdayPerson = document.querySelector("#birthdayPerson");
+    // 2. div giftCard
+    const giftCard = document.querySelector("#giftCard");
+    // 3. input birthdayName
+    const birthdayName = document.querySelector("#birthdayName");
+    // 4. input birthdayDate
+    const birthdayDate = document.querySelector("#birthdayDate");
+
+    // creating a new penguin
+    const penguin = new Penguin(birthdayPerson);
+
+
+
+})
